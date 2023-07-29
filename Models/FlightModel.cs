77ea018cbc7e2a97913494da6afecae641311022
Yussuf Shariff <yussufshariff@outlook.com>
2023-07-29 17:23:14 +0100

    Init

---
+++ Models/FlightModel.cs
@@ -0,0 +1,16 @@
+﻿namespace FlightAPI.Models
+{
+    public class FlightModel
+    {
+        public string FlightId { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public string Airline { get; set; }
+        public string Aircraft { get; set; }
+        public int AvailableSeats { get; set; }
+        public decimal Price { get; set; }
+        public TimeSpan Duration { get; set; }
+    }
+}
